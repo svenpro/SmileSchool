420365438ef981496dcce3ec142b6a7d8ae11c65
Sviatoslav Chyzh <slavik.chyzh@gmail.com>
2018-05-30 15:53:10 +0300

    homework-6.1

--- Project 1/js/widgets/menu-extend.js
+++ Project 1/js/widgets/menu-extend.js
@@ -8,19 +8,7 @@
         },
 
         _create: function () {
-
-            this.activeMenu = this.element;
-
-            // Flag used to prevent firing of the click handler
-            // as the event bubbles up through nested menus
-            this.mouseHandled = false;
-            this.element
-                .uniqueId()
-                .attr( {
-                    role: this.options.role,
-                    tabIndex: 0
-                } );
-
+            var self = this;
 
                 /// Function DEBOUNCE
             function debounce(func, wait, immediate) {
@@ -38,149 +26,40 @@
                 };
             }
 
+            var $window = $(window);
+            var windowsize = $window.width();
+
             $(document).ready(function() {
                 // Optimalisation: Store the references outside the event handler:
-                var $window = $(window);
-                var windowsize = $window.width();
+                var windowsize = $(window).width();
                 function checkWidth(windowsize) {
                     var windowsize = $window.width();
                     if (windowsize >= 1024) {
-                        this._desktopMenu();
+                        self._desktopMenu();
 
 
                     } else if (windowsize < 1023){
-                        this._mobileMenu();
+                        self._mobileMenu();
 
                     }
                 }
                 // Execute on load
                 checkWidth(windowsize);
                 // Bind event listener
-
                 $(window).on('resize', debounce(function () {
                     checkWidth($window.width());
                 }, 250));
             });
-
-            this._desktopMenu();
         },
 
         _desktopMenu: function() {
             console.log('desktop');
-            this.activeMenu = this.element;
 
-            // Flag used to prevent firing of the click handler
-            // as the event bubbles up through nested menus
-            this.mouseHandled = false;
-            this.element
-                .uniqueId()
-                .attr( {
-                    role: this.options.role,
-                    tabIndex: 0
-                } );
-
-            this._addClass( "ui-menu", "ui-widget ui-widget-content" );
-            this._on( {
-
-                // Prevent focus from sticking to links inside menu after clicking
-                // them (focus should always stay on UL during navigation).
-                "mousedown .ui-menu-item": function( event ) {
-                    event.preventDefault();
-                },
-                "click .ui-menu-item": function( event ) {
-                    var target = $( event.target );
-                    var active = $( $.ui.safeActiveElement( this.document[ 0 ] ) );
-                    if ( !this.mouseHandled && target.not( ".ui-state-disabled" ).length ) {
-                        this.select( event );
-
-                        // Only set the mouseHandled flag if the event will bubble, see #9469.
-                        if ( !event.isPropagationStopped() ) {
-                            this.mouseHandled = true;
-                        }
-
-                        // Open submenu on click
-                        if ( target.has( ".ui-menu" ).length ) {
-                            this.expand( event );
-                        } else if ( !this.element.is( ":focus" ) &&
-                            active.closest( ".ui-menu" ).length ) {
-
-                            // Redirect focus to the menu
-                            this.element.trigger( "focus", [ true ] );
-
-                            // If the active item is on the top level, let it stay active.
-                            // Otherwise, blur the active item since it is no longer visible.
-                            if ( this.active && this.active.parents( ".ui-menu" ).length === 1 ) {
-                                clearTimeout( this.timer );
-                            }
-                        }
-                    }
-                },
-                "mouseenter .ui-menu-item": function( event ) {
-
-                    // Ignore mouse events while typeahead is active, see #10458.
-                    // Prevents focusing the wrong item when typeahead causes a scroll while the mouse
-                    // is over an item in the menu
-                    if ( this.previousFilter ) {
-                        return;
-                    }
-
-                    var actualTarget = $( event.target ).closest( ".ui-menu-item" ),
-                        target = $( event.currentTarget );
-
-                    // Ignore bubbled events on parent items, see #11641
-                    if ( actualTarget[ 0 ] !== target[ 0 ] ) {
-                        return;
-                    }
-
-                    // Remove ui-state-active class from siblings of the newly focused menu item
-                    // to avoid a jump caused by adjacent elements both having a class with a border
-                    this._removeClass( target.siblings().children( ".ui-state-active" ),
-                        null, "ui-state-active" );
-                    this.focus( event, target );
-                },
-                mouseleave: "collapseAll",
-                "mouseleave .ui-menu": "collapseAll",
-                focus: function( event, keepActiveItem ) {
-
-                    // If there's already an active item, keep it active
-                    // If not, activate the first item
-                    var item = this.active || this.element.find( this.options.items ).eq( 0 );
-
-                    if ( !keepActiveItem ) {
-                        this.focus( event, item );
-                    }
-                },
-                blur: function( event ) {
-                    this._delay( function() {
-                        var notContained = !$.contains(
-                            this.element[ 0 ],
-                            $.ui.safeActiveElement( this.document[ 0 ] )
-                        );
-                        if ( notContained ) {
-                            this.collapseAll( event );
-                        }
-                    } );
-                },
-                keydown: "_keydown"
-            } );
-
-            this.refresh();
-
-            // Clicks outside of a menu collapse any open menus
-            this._on( this.document, {
-                click: function( event ) {
-                    if ( this._closeOnDocumentClick( event ) ) {
-                        this.collapseAll( event );
-                    }
-
-                    // Reset the mouseHandled flag
-                    this.mouseHandled = false;
-                }
-            } );
         },
 
         _mobileMenu: function() {
             console.log('mobile');
+
         }
 
     });
