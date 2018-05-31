c2090979e31e67d7ea56b9292a62205435d56cdb
Sviatoslav Chyzh <slavik.chyzh@gmail.com>
2018-05-31 15:09:24 +0300

    homework-6.3

--- Project 1/js/widgets/menu-extend.js
+++ Project 1/js/widgets/menu-extend.js
@@ -54,11 +54,54 @@
 
         _desktopMenu: function() {
             console.log('desktop');
+            $('footer').removeClass();
+            $('footer').addClass('main-footer');
+            $( ".menu" ).menu( "disable" );
+            $( ".menu" ).menu( "enable" );
 
         },
 
         _mobileMenu: function() {
             console.log('mobile');
+
+            $('footer').removeClass().addClass('footer-mobile');
+
+            $('.sub-menu').removeAttr('style');
+
+            $('.ui-menu-item-wrapper.ui-state-active').next('.sub-menu').css('color', 'red');
+
+            var toggleMenu = $('.menu');
+
+            $('.burger-menu').bind('click', function() {
+
+                if($(this).attr('data-click-state') == 1) {
+                    $(this).attr('data-click-state', 0);
+                    toggleMenu.css('display', 'none');
+                    $('.btn-close').css('display', 'none');
+                    $('body').css('margin-left', '');
+                } else {
+                    $(this).attr('data-click-state', 1);
+                    toggleMenu.css('display', 'block');
+                    $('.btn-close').css('display', 'block');
+                    $('body').css('margin-left', '40%');
+                }
+            });
+
+
+            $('.btn-close').on('click', function(){
+                $('.menu').css('display', 'none');
+                $(this).css('display', 'none');
+                $('.burger-menu').attr('data-click-state', 0);
+                $('body').css('margin-left', '');
+            });
+
+            $('.ui-menu-icon').on('click', function(){
+                console.log('button click');
+                $('.sub-menu').toggle(500, function() {
+                    $(this).css('display', 'block')
+                });
+
+            });
 
         }
 
