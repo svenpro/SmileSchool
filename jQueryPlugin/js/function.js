ac5136698529b95e8be61e74cafd1776f2b093c9
anrav <anrav@smile.fr>
2018-05-31 11:42:54 +0300

    add plugin lesson

---
+++ jQueryPlugin/js/function.js
@@ -0,0 +1,34 @@
+(function ($) {
+    var accordion =  $('[data-role=accordion]');
+
+    $('.back-to-top').backToTop({
+        'speed': 9000,
+        'scrollTriggerHeight': 50
+    });
+
+    accordion.accordionExtend({
+        active: 0,
+        header: ".accordion-title",
+        titleColor: 'green',
+        classes: {
+            "ui-accordion-header": "accordion-title-olol",
+            "ui-accordion-content": "accordion-content"
+        }
+    });
+
+    $('.destroy').on('click', function () {
+        accordion.accordionExtend( "destroy" );
+    });
+
+    $('.enable').on('click', function () {
+        accordion.accordionExtend( "enable" );
+    });
+
+    $('.disable').on('click', function () {
+        accordion.accordionExtend( "disable" );
+    });
+
+    $('document').ready(function () {
+        accordion.newWidget();
+    })
+})(jQuery);
