a4aabfcb84c72267ef9c826238df40d9dec876c5
Sviatoslav Chyzh <slavik.chyzh@gmail.com>
2018-06-18 15:13:55 +0300

    homework-10.1

--- AJAX/lib/widgets/updateTable.js
+++ AJAX/lib/widgets/updateTable.js
@@ -45,16 +45,39 @@ define([
         newAddress: function () {
             var self = this;
             var element = this.element;
+            var dialog = $("#dialog");
 
-
-            // console.log(self.ajaxData);
             $.each(self.ajaxData, function(key, value) {
                 console.log(value.id);
                 // element.find('td:last').html("<button class='address"+value.id+"'>Address</button>");
                 $(".address"+value.id).on('click', function() {
                     console.log(value);
+                    dialog.dialog( "open" );
+
+                    var tableTR = $('<tr>');
+                    $.each(value, function (key, row) {
+                        var tableTd = '';
+                        tableTd = $('<td>').text(row);
+                        tableTR.append(tableTd);
+                    });
+                    var addrTable = "";
+                    $(".tableDialog").append(tableTR);
                 });
             });
+
+            dialog.dialog({
+                autoOpen: false,
+                show: {
+                    effect: "blind",
+                    duration: 500
+                },
+                hide: {
+                    effect: "explode",
+                    duration: 500
+                }
+            });
+
+
             //
             // $.each(self.ajaxData, function(key, value) {
             //     element.find('td:last').html("<button class='address"+value.id+"'>Address</button>");
